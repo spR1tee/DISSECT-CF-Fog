858ea751cf35030f394f178deb7b76cafb5a13a7
Eliot Nagy <nagyeliot@gmail.com>
2024-12-17 14:48:51 +0100

    request handling

--- simulator/src/main/java/hu/u_szeged/inf/fog/simulator/spring/SimulatorController.java
+++ simulator/src/main/java/hu/u_szeged/inf/fog/simulator/spring/SimulatorController.java
@@ -2,7 +2,7 @@ package hu.u_szeged.inf.fog.simulator.spring;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hu.u_szeged.inf.fog.simulator.spring.model.VmData;
-import hu.u_szeged.inf.fog.simulator.spring.model.VmInstance;
+import hu.u_szeged.inf.fog.simulator.spring.service.SimulatorService;
 import hu.u_szeged.inf.fog.simulator.spring.service.VmInstanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,12 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("/simulator")
 public class SimulatorController {
 
+    @Autowired
+    private SimulatorService simulatorService;
     @Autowired
     private VmInstanceService vmInstanceService;
 
@@ -26,27 +27,12 @@ public class SimulatorController {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             VmData vmData = objectMapper.readValue(jsonContent, VmData.class);
-            createVm(vmData);
+            simulatorService.handleRequest(vmData);
             return ResponseEntity.ok("Data has been processed.");
         } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.status(500).body("Error while processing data.");
         }
-    }
-
-    private void createVm(VmData vmData) {
-        List<VmInstance> vmInstances = vmData.getVmInstances();
-        /*for (VmInstance vmInstance : vmInstances) {
-            vmInstanceService.createVmInstance(vmInstance);
-        }*/
-        List<VmInstance> query = vmInstanceService.getAllVmInstances();
-        for (VmInstance vmInstance : query) {
-            /*if (vmInstance.getId() != null) {
-                vmInstanceService.deleteVmInstance(vmInstance.getId());
-            }*/
-            System.out.println(vmInstance.getId() + " " + vmInstance.getName());
-        }
-        // TO DO
     }
 }
 
--- simulator/src/main/java/hu/u_szeged/inf/fog/simulator/spring/service/SimulatorService.java
+++ simulator/src/main/java/hu/u_szeged/inf/fog/simulator/spring/service/SimulatorService.java
@@ -1,5 +1,10 @@
 package hu.u_szeged.inf.fog.simulator.spring.service;
 
+import hu.mta.sztaki.lpds.cloud.simulator.iaas.constraints.AlterableResourceConstraints;
+import hu.mta.sztaki.lpds.cloud.simulator.io.VirtualAppliance;
+import hu.u_szeged.inf.fog.simulator.provider.Instance;
+import hu.u_szeged.inf.fog.simulator.spring.model.VmData;
+import hu.u_szeged.inf.fog.simulator.spring.model.VmInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -8,14 +13,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class SimulatorService {
 
     @Autowired
     private VmInstanceService vmInstanceService;
 
+    private final List<VirtualAppliance> virtualAppliances = new ArrayList<>();
+    private final List<Instance> instances = new ArrayList<>();
     private final RestTemplate restTemplate = new RestTemplate();
     private final HttpHeaders headers = new HttpHeaders();
+    private int counter = 0;
 
     public String sendData(String data, String url) {
 
@@ -27,5 +38,29 @@ public class SimulatorService {
         return response.getBody();
     }
 
+    public void handleRequest(VmData vmData) {
+        String request_type = vmData.getRequestType().toUpperCase();
+        if (request_type.equals("UPDATE")) {
+            if (Instance.allInstances.isEmpty()) {
+                for (VmInstance vmInstance : vmData.getVmInstances()) {
+                    virtualAppliances.add(new VirtualAppliance("va" + counter, vmInstance.getStartupProcess(),0, false, vmInstance.getReqDisk()));
+                    AlterableResourceConstraints arc = new AlterableResourceConstraints(vmInstance.getCpu(), vmInstance.getCoreProcessingPower(), vmInstance.getRam());
+                    instances.add(new Instance("instance" + counter, virtualAppliances.get(counter), arc, vmInstance.getPricePerTick() / 60 / 60 / 1000));
+                    counter++;
+                }
+            } else {
+                // TO DO update the values of the instances if needed
+                for (Instance instance : instances) {
+                    System.out.println(instance);
+                }
+            }
+        } else if (request_type.equals("REQUEST PREDICTION")) {
+            // TO DO call prediction functions
+        } else if (request_type.equals("REQUEST FUTURE BEHAVIOR")) {
+            //TO DO
+        } else {
+            System.err.println("Error: Unknown Request Type");
+        }
+    }
 }
 
